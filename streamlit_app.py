45dd886ea582aac5a14468b704350c9641a5568f
charisaoliviaa <charisaolivia84@gmail.com>
2025-05-20 08:15:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title("Charisa FITB-ITB")
 st.write(
     "Yuk berdoa dan belajar bareng niat masuk itb"
 )
-st.image("IMG-20250502-WA0012.jpg", width=200)
+st.image("IMG-20250423-WA0033.jpg", width=200)
