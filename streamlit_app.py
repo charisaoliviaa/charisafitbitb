9fb70d6f533b33a42715e4aa43cadb5bf8522e0f
charisaoliviaa <charisaolivia84@gmail.com>
2025-05-27 07:52:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,13 @@ st.write(
     "Yuk berdoa dan belajar bareng niat masuk itb"
 )
 st.image("3e27ae0aaf708820d0ff0225fe3e1085-removebg-preview.png",width=200)
+
+
+st.title("Aplikasi Sederhana")
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
+angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
+
+if (angka % 2) == 0:
+    st.write(f"{angka} adalah Bilangan Genap")
+else: 
+st.write(f"{angka} adalah Bilangan Ganjil")
